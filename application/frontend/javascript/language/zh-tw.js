d58b1063fc9c651c38b14a0aa2fdaf48f77ee852
Kelp <kelp@phate.org>
2014-06-16 11:51:22 +0800

    added language resources.

--- application/frontend/javascript/language/zh-tw.js
+++ application/frontend/javascript/language/zh-tw.js
@@ -1,7 +1,16 @@
 (function() {
   window.languageResource = {
     'Salmon': 'Salmon',
-    'Salmon issue tracker': 'Salmon 項目追蹤'
+    'Salmon issue tracker': 'Salmon 項目追蹤',
+    'Sign in': '登入',
+    'Sign out': '登出',
+    'Settings': '設定',
+    'Profile': '帳號',
+    'Projects': '專案',
+    'Users': '使用者',
+    'Name': '姓名',
+    'Email': '信箱',
+    'Save': '儲存'
   };
 
 }).call(this);
--- application/frontend/language/zh-tw.coffee
+++ application/frontend/language/zh-tw.coffee
@@ -1,3 +1,12 @@
 window.languageResource =
     'Salmon': 'Salmon'
     'Salmon issue tracker': 'Salmon 項目追蹤'
+    'Sign in': '登入'
+    'Sign out': '登出'
+    'Settings': '設定'
+    'Profile': '帳號'
+    'Projects': '專案'
+    'Users': '使用者'
+    'Name': '姓名'
+    'Email': '信箱'
+    'Save': '儲存'
