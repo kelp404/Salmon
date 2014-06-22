40372e1eddf40bf3ab91686dc92602c934604f09
Kelp <kelp@phate.org>
2014-06-22 18:04:56 +0800

    fixed allow tags for bleach.

--- application/utils.py
+++ application/utils.py
@@ -27,7 +27,7 @@ def get_bleach_allow_tags():
     return [
         'hr',
         'img',
-        'p',
+        'p', 'span',
         'a',
         'del',
         'br',
@@ -41,6 +41,7 @@ def get_bleach_allow_tags():
         'strong',
         'ul',
         'div'
+        'h1', 'h2', 'h3', 'h4', 'h5',
     ]
 def get_bleach_allow_attributes():
     return {
