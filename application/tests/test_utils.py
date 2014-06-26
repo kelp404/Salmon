d27887e12d70d24756015fa75e3b68f9c7ca0592
Kelp <kelp@phate.org>
2014-06-26 12:39:05 +0800

    added tests for utils.

--- application/tests/test_utils.py
+++ application/tests/test_utils.py
@@ -5,12 +5,68 @@ from application import utils
 
 
 class TestUtilsModels(unittest.TestCase):
+    def setUp(self):
+        self.request = MagicMock()
+
     @patcher(
         patch('django.conf.settings.DEBUG', new_callable=MagicMock(return_value='debug')),
     )
     def test_utils_is_debug(self):
         is_debug = utils.is_debug()
         self.assertEqual(is_debug, 'debug')
+
+    def test_utils_current_language_zh_tw(self):
+        self.request.META = {
+            'HTTP_ACCEPT_LANGUAGE': 'zh-TW,zh;q=0.8,en;q=0.6'
+        }
+        code = utils.current_language(self.request)
+        self.assertEqual(code, 'zh-tw')
+    def test_utils_current_language_zh_hk(self):
+        self.request.META = {
+            'HTTP_ACCEPT_LANGUAGE': 'zh-HK,zh;q=0.8,en;q=0.6'
+        }
+        code = utils.current_language(self.request)
+        self.assertEqual(code, 'zh-tw')
+    def test_utils_current_language_ja_jp(self):
+        self.request.META = {
+            'HTTP_ACCEPT_LANGUAGE': 'ja-JP,en;q=0.6'
+        }
+        code = utils.current_language(self.request)
+        self.assertEqual(code, 'en')
+    def test_utils_current_language_en(self):
+        self.request.META = {
+            'HTTP_ACCEPT_LANGUAGE': 'en'
+        }
+        code = utils.current_language(self.request)
+        self.assertEqual(code, 'en')
+
+    def test_utils_parse_keyword(self):
+        keyword = 'apple -banana index'
+        plus, minus = utils.parse_keyword(keyword)
+        self.assertListEqual(plus, [
+            'apple',
+            'index',
+        ])
+        self.assertListEqual(minus, [
+            'banana'
+        ])
+
+    @patcher(
+        patch('application.utils.search', new_callable=MagicMock()),
+    )
+    def test_utils_get_index_users(self):
+        utils.search.Index.return_value = 'index'
+        index = utils.get_index_users()
+        self.assertEqual(index, 'index')
+        utils.search.Index.assert_called_with(namespace='UserModel', name='default')
+    @patcher(
+        patch('application.utils.search', new_callable=MagicMock()),
+    )
+    def test_utils_get_index_issues(self):
+        utils.search.Index.return_value = 'index'
+        index = utils.get_index_issues(100)
+        self.assertEqual(index, 'index')
+        utils.search.Index.assert_called_with(namespace='IssueModel', name='100')
 
     def test_utils_get_iso_format(self):
         dt = MagicMock()
